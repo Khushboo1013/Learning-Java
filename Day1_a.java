94e37b43f2ff7617ec8ba1a60345c97eb30c7455
Khushboo Yadav <112149305+Khushboo1013@users.noreply.github.com>
2023-02-02 19:22:27 +0530

    Add files via upload

---
+++ Day1_a.java
@@ -0,0 +1,14 @@
+public class Day1_a {
+    public static void main(String args[]){
+        // we have variable(value can vary) and literals(value cannot vary)
+        int a=12; //here a is a variable , its value vary   --int is a data type
+        a=13;
+        int b=16;
+        b=a;
+        String name ="Khushboo Yadav";
+        System.out.println("a");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+    }
+}
