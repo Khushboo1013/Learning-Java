d939f90379b2863dc5310fc9189ea8faaf3e0ac7
Khushboo Yadav <112149305+Khushboo1013@users.noreply.github.com>
2023-02-02 19:00:59 +0530

    Add files via upload

---
+++ Day1Feb2_Basic.java
@@ -0,0 +1,17 @@
+public class Day1Feb2_Basic {
+    public static void main(String args[]){
+        /*
+        Print Pattern
+     *****
+     ****
+     ***
+     **
+     *
+     */
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*"); // ; - this is statement terminater
+    }
+}
