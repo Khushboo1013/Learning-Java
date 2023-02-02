9e59ad021b9dba290e675b225b12a3bcfc562a21
Khushboo Yadav <112149305+Khushboo1013@users.noreply.github.com>
2023-02-02 19:44:53 +0530

    Add files via upload

---
+++ Day1_b.java
@@ -0,0 +1,27 @@
+public class Day1_b {
+    /*
+        Java is a typed language , that means in this lang. we have to define the data type
+       Data types in java
+       1) Primitive Data types(already exist) in bytes |  2) Non -Primitive in bytes
+            byte (1 Byte [-128 to 127])                |        String
+            short (2 Byte)                             |        Array
+            char (2 Bytes)                               |       Class
+            boolean (1 byte)                            |       Object
+            int   (4 bytes)                              |       Interface
+            long  (8 bytes)
+            float(3.24)(4 bytes)
+            double  (8 bytes)
+     */
+    public static void main(String args[]){
+        byte b=3;
+        System.out.println(b);
+        char ch='b';
+        System.out.println(ch);
+        boolean var=true;
+        float price=4.5f;
+        int num=25;
+        long largenum=234;
+        short n=213;
+        System.out.println(var);
+    }
+}
