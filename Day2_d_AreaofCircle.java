03205d4183ddd1587ddd62796c500bcc9ddc75e4
Khushboo Yadav <112149305+Khushboo1013@users.noreply.github.com>
2023-02-03 18:25:45 +0530

    Add files via upload

---
+++ Day2_d_AreaofCircle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Day2_d_AreaofCircle {
+    public static void main(String args[]){
+        Scanner sca=new Scanner(System.in);
+        float rad=sca.nextFloat();
+        float area=3.14f*rad*rad;
+        System.out.println(area);
+    }
+}
