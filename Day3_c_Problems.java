3c1596669b690a67782d7b280cf1d6d6653cd699
Khushboo Yadav <112149305+Khushboo1013@users.noreply.github.com>
2023-02-04 10:54:41 +0530

    Add files via upload

---
+++ Day3_c_Problems.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Day3_c_Problems {
+    public static void main(String[] args) {
+        //Ques 1: In a program , i/p 3 numbers:A,B and C, and give o/p average.
+        Scanner sc=new Scanner(System.in);
+        /*
+        int A=sc.nextInt();
+        int B=sc.nextInt();
+        int C=sc.nextInt();
+        int D=(A+B+C)/3;
+        System.out.println(D);
+        */
+        
+        // Quest 2: i/p a no. and find the area of sq.
+        /* 
+        int side=sc.nextInt();
+        int area_sq=side*side;
+        System.out.println(area_sq);
+        */
+        
+        // Question 3: Enter cost of 3 items from user and give total bill with 18% gst
+        /*
+        float pen=sc.nextFloat();
+        float pencil=sc.nextFloat();
+        float eraser=sc.nextFloat();
+        float cost=(pen+pencil+eraser);
+        float bill =cost + (cost*.18f)
+        System.out.println(cost);
+        System.out.println(bill);
+        */
+
+        int $=12; // does not show any error
+
+    }
+}
