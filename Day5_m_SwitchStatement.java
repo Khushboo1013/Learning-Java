f9626f894a087a90df8ce34b0920e84a74ab0293
Khushboo Yadav <112149305+Khushboo1013@users.noreply.github.com>
2023-02-06 18:52:50 +0530

    Add files via upload

---
+++ Day5_m_SwitchStatement.java
@@ -0,0 +1,30 @@
+public class Day5_m_SwitchStatement {
+    public static void main(String[] args) {
+        /*
+        switch(variable){
+            case 1:
+
+            case 2:
+
+            case 3:
+
+            default:
+        }
+        */
+        int num=12;
+        switch(num){
+            case 1: System.out.println("Haaaa");
+            break;
+            case 2:System.out.println("Yeeeeee");
+            break;
+            case 12:System.out.println("Hello");
+            break;
+            case 3:System.out.println("ohhhhhh");
+            break;
+            case 38:System.out.println("Noooooo");
+            break;
+            default:
+            System.out.println(":}");
+        }
+    }
+}
