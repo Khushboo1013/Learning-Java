199edc2d75288253195d728bd7bcf3dc0e53ce1b
Khushboo Yadav <112149305+Khushboo1013@users.noreply.github.com>
2023-02-06 17:38:05 +0530

    Add files via upload

---
+++ Day5_f_PrintLargestOf2.java
@@ -0,0 +1,11 @@
+public class Day5_f_PrintLargestOf2 {
+    public static void main(String[] args) {
+        int a=4;
+        int b=7;
+        if(a>b){
+            System.out.println("a is greater");
+        }else{
+            System.out.println("b is greater");
+        } 
+    }
+}
