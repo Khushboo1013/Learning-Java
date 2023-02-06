f6b8c43978dcbc6d0dc1bcbc6cfb381802cb52a2
Khushboo Yadav <112149305+Khushboo1013@users.noreply.github.com>
2023-02-06 15:04:03 +0530

    Add files via upload

---
+++ Day5_d_assignOperator.java
@@ -0,0 +1,19 @@
+public class Day5_d_assignOperator {
+    public static void main(String[] args) {
+        /*
+          Assignment Operator
+           
+          =        a=b;
+          +=       a=a+10(a+=10); 
+          -=       B=B-5(B-=5);
+          *=
+          /=
+        */
+        int A=20;
+        A+=45;  //A=A+45;
+        int B=10;
+        B-=5;  //B=B-5;
+        System.out.println(A);
+        System.out.println(B);
+    }
+}
