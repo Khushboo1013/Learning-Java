8cac99a1be8aa13c57be16c255c4c38f63ee53d4
Khushboo Yadav <112149305+Khushboo1013@users.noreply.github.com>
2023-02-06 14:08:43 +0530

    Add files via upload

---
+++ Day5_b_RelatOperator.java
@@ -0,0 +1,21 @@
+public class Day5_b_RelatOperator {
+    public static void main(String[] args) {
+        /*
+           Relational Operators(check relation)
+           ==  equal to
+           !=  not equal to
+           >   greater than
+           <   less than
+           >=  greater than equal to
+           <=  smaller than equal to
+        */
+        int a =10;
+        int b=2;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>=b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a<b);
+    }
+}
