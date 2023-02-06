9e76a1198438a837e62cd44cb88b7b363545edc4
Khushboo Yadav <112149305+Khushboo1013@users.noreply.github.com>
2023-02-06 17:37:44 +0530

    Add files via upload

---
+++ Day5_e_ConditionalStatement.java
@@ -0,0 +1,25 @@
+public class Day5_e_ConditionalStatement {
+    public static void main(String args[]) {
+        /* 
+          conditional statements
+
+          if,else 
+          if(condition){
+
+          }else{
+
+          }
+          
+          else if
+          ternery operator
+          switch
+        */
+        int myage=21;
+        if(myage>18){
+            System.out.println("I am able to vote now");
+        }
+        else{
+            System.out.println("I am not able to vote now");
+        }
+    }
+}
