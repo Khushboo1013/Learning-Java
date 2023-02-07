21b9cda6fdec045458fca93f8aa770f462b533db
Khushboo Yadav <112149305+Khushboo1013@users.noreply.github.com>
2023-02-07 19:31:13 +0530

    Add files via upload

---
+++ Day6_i_doWhile.java
@@ -0,0 +1,14 @@
+public class Day6_i_doWhile {
+    public static void main(String[] args) {
+        /*
+          do{
+            //do something
+          }while(condion);
+        */
+        int counter=1;
+        do{
+            System.out.println("Hello World");
+            counter++;
+        }while(counter<=10);
+    }
+}
