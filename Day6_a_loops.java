6df169ae79836303f066ff24a0574e119b50c498
Khushboo Yadav <112149305+Khushboo1013@users.noreply.github.com>
2023-02-07 18:27:06 +0530

    Add files via upload

---
+++ Day6_a_loops.java
@@ -0,0 +1,22 @@
+public class Day6_a_loops {
+    public static void main(String args[]){
+        /*
+         loop
+         for loop
+         while loop
+         
+         while(condition){
+            //do something
+         }
+         Do while
+        */
+
+        // Print hello world 10 times by using while loop
+        int counter=0;
+        while(counter<10){
+            System.out.println("Hello world");
+            counter++;
+        }
+        System.out.println("I am back");
+    }
+}
