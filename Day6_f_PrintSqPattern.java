151131d22b277b28e0eb01bd04d0379bb92e2a3d
Khushboo Yadav <112149305+Khushboo1013@users.noreply.github.com>
2023-02-07 19:03:41 +0530

    Add files via upload

---
+++ Day6_f_PrintSqPattern.java
@@ -0,0 +1,9 @@
+public class Day6_f_PrintSqPattern {
+    public static void main(String[] args) {
+        //  print Square pattern
+        int num=4;
+        for(int n=1;n<=num;n++){
+            System.out.println("* * * *");
+        }
+    }
+}
