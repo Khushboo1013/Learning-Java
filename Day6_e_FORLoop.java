b0c2f0c1f3d47c31637090d5fd2e807aa8baf607
Khushboo Yadav <112149305+Khushboo1013@users.noreply.github.com>
2023-02-07 18:55:02 +0530

    Add files via upload

---
+++ Day6_e_FORLoop.java
@@ -0,0 +1,12 @@
+public class Day6_e_FORLoop {
+    public static void main(String[] args) {
+        /*
+          for(initialisation ;condition ;updation){
+            // do something
+          }
+        */
+        for(int i=1;i<=10;i++){
+            System.out.println("Hello World");
+        }
+    }
+}
